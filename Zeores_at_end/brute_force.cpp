e828b4491deae967b98a5527efd20425271ff649
Hardik Mittal <hardiksakshammittal1307@gmail.com>
2026-09-06 14:06:03 +0530

    Moving Zeroes to End of Array

---
+++ Zeores_at_end/brute_force.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void zeroesAtEnd(vector<int> &arr,int n){
+    vector<int> temp;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            temp.push_back(arr[i]);
+        }
+    }
+    for(int i=0;i<temp.size();i++){
+        arr[i]=temp[i];
+    }
+    for(int i=temp.size();i<n;i++){
+        arr[i]=0;
+    }
+}
+int main(){
+int n;
+cin>>n;
+vector<int> arr(n);
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+zeroesAtEnd(arr,n);
+for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+}
+return 0;
+}
