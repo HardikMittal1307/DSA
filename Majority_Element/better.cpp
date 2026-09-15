4e059f857f02a6978c213c073d6512d67cbffdde
Hardik Mittal <hardiksakshammittal1307@gmail.com>
2026-09-15 19:46:00 +0530

    Finding Majority Element from an Array

---
+++ Majority_Element/better.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int majorElement(int arr[],int n){
+    map<int,int>mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]++;
+    }
+    for(auto it:mpp){
+        if(it.second>n/2){
+            return it.first;
+        }
+    }
+    return -1;
+}
+int main(){
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+int major_element=majorElement(arr,n);
+cout<<major_element;
+return 0;
+}
